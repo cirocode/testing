c744baa91139ee07c0b0fbc1790a3b6924bb70c7
Cesarino Ijak Rosa <cesarinoijakrosa@gmail.com>
2019-01-14 21:49:03 +0900

    just koko

---
+++ app/src/main/java/com/example/cirolab/webview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cirolab.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewLebakCyber);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://www.kokodt.tk");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
+}
